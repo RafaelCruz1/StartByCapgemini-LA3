faf678316c9b4373c2bba24f38a6ea88a4293ea9
RafaelCruz1 <rafaeloribata@gmail.com>
2023-02-09 12:33:05 -0300

    Criando a classe project

---
+++ src/Model/Project.java
@@ -0,0 +1,73 @@
+package Model;
+
+import java.util.Date;
+
+public class Project {
+    private Integer id;
+    private String name;
+    private String description;
+    private Date createdAt;
+    private Date updateAt;
+
+    public Project() {
+    }
+
+    public Project(Integer id, String name, String description, Date createdAt, Date updateAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", createdAt=" + createdAt +
+                ", updateAt=" + updateAt +
+                '}';
+    }
+}
